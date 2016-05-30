32ae9a7dbfdea16bd567d47d55dbba801c161654
BKaaa <botond.kis@allaboutapps.at>
2016-05-30 16:09:58 +0200

    added helper function to draw ship

--- BattleshipAdvanced/ABMapSprites.cpp
+++ BattleshipAdvanced/ABMapSprites.cpp
@@ -5,27 +5,27 @@
 void drawHorizontalShip(ABPoint origin, byte shipLength, byte color){
 
   if(shipLength == 1){
-    arduboy.drawBitmap(origin.x, origin.y, BAMap_Sprite_Ship_horizontal_single, 8, 8, WHITE);
+    arduboy.drawBitmap(origin.x, origin.y, BAMap_Sprite_Ship_horizontal_single, 8, 8, color);
   }
   else if (shipLength >= 2){
-    arduboy.drawBitmap(origin.x, origin.y, BAMap_Sprite_Ship_horizontal_1, 8, 8, WHITE);
-    arduboy.drawBitmap(origin.x + ((shipLength-1)*8), origin.y, BAMap_Sprite_Ship_horizontal_3, 8, 8, WHITE);
+    arduboy.drawBitmap(origin.x, origin.y, BAMap_Sprite_Ship_horizontal_3, 8, 8, color);
+    arduboy.drawBitmap(origin.x + ((shipLength-1)*8), origin.y, BAMap_Sprite_Ship_horizontal_1, 8, 8, color);
     
     for(byte i = 1; i < (shipLength-1); i++)
-      arduboy.drawBitmap(origin.x + (i*8), origin.y, BAMap_Sprite_Ship_horizontal_2, 8, 8, WHITE);
+      arduboy.drawBitmap(origin.x + (i*8), origin.y, BAMap_Sprite_Ship_horizontal_2, 8, 8, color);
   }
 }
 
 void drawVerticalShip(ABPoint origin, byte shipLength, byte color){
   if(shipLength == 1){
-    arduboy.drawBitmap(origin.x, origin.y, BAMap_Sprite_Ship_vertical_single, 8, 8, WHITE);
+    arduboy.drawBitmap(origin.x, origin.y, BAMap_Sprite_Ship_vertical_single, 8, 8, color);
   }
   else if (shipLength >= 2){
-    arduboy.drawBitmap(origin.x, origin.y, BAMap_Sprite_Ship_vertical_1, 8, 8, WHITE);
-    arduboy.drawBitmap(origin.x, origin.y + ((shipLength-1)*8), BAMap_Sprite_Ship_vertical_3, 8, 8, WHITE);
+    arduboy.drawBitmap(origin.x, origin.y, BAMap_Sprite_Ship_vertical_1, 8, 8, color);
+    arduboy.drawBitmap(origin.x, origin.y + ((shipLength-1)*8), BAMap_Sprite_Ship_vertical_3, 8, 8, color);
     
     for(byte i = 1; i < (shipLength-1); i++)
-      arduboy.drawBitmap(origin.x, origin.y+ (i*8), BAMap_Sprite_Ship_vertical_2, 8, 8, WHITE);
+      arduboy.drawBitmap(origin.x, origin.y+ (i*8), BAMap_Sprite_Ship_vertical_2, 8, 8, color);
   }
 }
 
