b7340c42982cf26f5a21888391ffa4406ebdea32
BKaaa <botond.kis@allaboutapps.at>
2016-05-30 09:18:09 +0200

    updated geometry funcs

--- BattleshipAdvanced/ABGeometry.h
+++ BattleshipAdvanced/ABGeometry.h
@@ -23,15 +23,15 @@ struct ABRect{
 // --------------------------------------------------------------
 // comparisson
 
-inline bool pointIsEqualToPoint(ABPoint a, ABPoint b){
+static inline bool pointIsEqualToPoint(ABPoint a, ABPoint b){
   return ((a.x == b.x) && (a.y == b.y));
 }
 
-inline bool sizeIsEqualToSize(ABSize a, ABSize b){
+static inline bool sizeIsEqualToSize(ABSize a, ABSize b){
   return ((a.width == b.width) && (a.height == b.height));
 }
 
-inline bool rectIsEqualToRect(ABRect a, ABRect b){
+static inline bool rectIsEqualToRect(ABRect a, ABRect b){
   return (pointIsEqualToPoint(a.origin, b.origin) && sizeIsEqualToSize(a.size, b.size));
 }
 
@@ -39,57 +39,46 @@ inline bool rectIsEqualToRect(ABRect a, ABRect b){
 // --------------------------------------------------------------
 // Factories
 
-inline ABPoint ABPointMake (int x, int y){
-  ABPoint p;
-  p.x = x;
-  p.y = y;
-  return p;
+static inline ABPoint ABPointMake (int x, int y){
+  return {x, y};
 }
 
-inline ABSize ABSizeMake (int width, int height){
-  ABSize s;
-  s.width = width;
-  s.height = height;
-  return s;
+static inline ABSize ABSizeMake (int width, int height){
+  return {width, height};
 }
 
-inline ABRect ABRectMake (int originX, int  originY, int width, int height){
-  ABRect r;
-  r.origin.x = originX;
-  r.origin.y = originY;
-  r.size.width = width;
-  r.size.height = height;
-  return r;
+static inline ABRect ABRectMake (int originX, int  originY, int width, int height){
+  return {{originX, originY}, {width, height}};
 }
 
 // --------------------------------------------------------------
 // Math helper
 
-inline bool ABRectMaxX (ABRect rect){
+static inline bool ABRectMaxX (ABRect rect){
   return rect.origin.x + rect.size.width;
 }
 
-inline bool ABRectMaxY (ABRect rect){
+static inline bool ABRectMaxY (ABRect rect){
   return rect.origin.y + rect.size.height;
 }
 
 // Basic collision detection
-inline bool ABRectContainsPoint ( ABRect rect, ABPoint point ){
+static inline bool ABRectContainsPoint ( ABRect rect, ABPoint point ){
   return (/*Horizontal*/ (point.x >= rect.origin.x && point.x <= ABRectMaxX(rect)) && /*Vertical*/ (point.y >= rect.origin.y && point.y <= ABRectMaxY(rect)));
 }
 
 // Basic collision detection
-inline bool ABRectIntersectsRect ( ABRect rect1, ABRect rect2 ){
+static inline bool ABRectIntersectsRect ( ABRect rect1, ABRect rect2 ){
   return (rect1.origin.x < ABRectMaxX(rect2) && ABRectMaxX(rect1) > rect2.origin.x && rect1.origin.y < ABRectMaxY(rect2) && ABRectMaxY(rect1)> rect2.origin.y);
 }
 
 // Returns a+b
-inline ABPoint addPoints(ABPoint a, ABPoint b){
+static inline ABPoint addPoints(ABPoint a, ABPoint b){
   ABPointMake(a.x + b.x, a.y + b.y);
 }
 
 // Returns a-b
-inline ABPoint substractPoints(ABPoint a, ABPoint b){
+static inline ABPoint substractPoints(ABPoint a, ABPoint b){
   ABPointMake(a.x - b.x, a.y - b.y);
 }
 
@@ -97,7 +86,7 @@ inline ABPoint substractPoints(ABPoint a, ABPoint b){
 // Animators
 
 // returns a point between the start and endpoint where the progress must be between 0 and 1
-inline ABPoint animatePointFromToPoint(ABPoint startPoint, ABPoint endPoint, float progress){
+static inline ABPoint animatePointFromToPoint(ABPoint startPoint, ABPoint endPoint, float progress){
   ABPoint result;
 
   result.x = startPoint.x + fabs( ((float)(startPoint.x - endPoint.x))*progress + 0.5f); // 0.5 for correct rounding
@@ -107,7 +96,7 @@ inline ABPoint animatePointFromToPoint(ABPoint startPoint, ABPoint endPoint, flo
 }
 
 // returns a size between the start and endsize where the progress must be between 0 and 1
-inline ABSize animateSizeToSize(ABSize startSize, ABSize endSize, float progress){
+static inline ABSize animateSizeToSize(ABSize startSize, ABSize endSize, float progress){
   ABSize result;
 
   result.width  = startSize.width   - float( ((float)(startSize.width - endSize.width))*progress + 0.5f); // 0.5 for correct rounding
@@ -117,7 +106,7 @@ inline ABSize animateSizeToSize(ABSize startSize, ABSize endSize, float progress
 }
 
 // returns a rect between the start and endrect where the progress must be between 0 and 1
-inline ABRect animateRectToRect(ABRect startRect, ABRect endRect, float progress){
+static inline ABRect animateRectToRect(ABRect startRect, ABRect endRect, float progress){
   ABRect result;
 
   result.origin = animatePointFromToPoint(startRect.origin, endRect.origin, progress);
