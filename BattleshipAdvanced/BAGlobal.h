ec38d4060cf8a8b0fe5216b74c4c1e276f184490
BKaaa <botond.kis@allaboutapps.at>
2016-05-30 13:07:32 +0200

    added menu width to global

--- BattleshipAdvanced/BAGlobal.h
+++ BattleshipAdvanced/BAGlobal.h
@@ -6,10 +6,11 @@
 #include "BASounds.h"
 #include "ABGeometry.h"
 
-
 // In 8x8 blocks
 #define GAME_BOARD_SIZE_WIDTH 12
 #define GAME_BOARD_SIZE_HEIGHT 8
+
+#define MENU_WIDTH 32
 
 extern Arduboy arduboy;
 extern BAInput globalInput;
