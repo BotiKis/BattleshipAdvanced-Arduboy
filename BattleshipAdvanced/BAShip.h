4c4a589ed7bf6229d4c8c61a8231668806688efe
BKaaa <botond.kis@allaboutapps.at>
2016-05-30 13:07:43 +0200

    added ships classes

---
+++ BattleshipAdvanced/BAShip.h
@@ -0,0 +1,25 @@
+#ifndef BASHIP_H
+#define BASHIP_H
+#include <Arduboy.h>
+
+typedef enum BAShipType{
+  BAShipTypeSmall = 1,
+  BAShipTypeMedium,
+  BAShipTypeBig
+}BAShipType;
+
+struct BAShip{
+  BAShipType shipType;
+  byte remainingLength;
+  bool horizontal;
+};
+
+inline BAShip BAShipMake(BAShipType shipType, byte shipLength){
+  return  {shipType, shipLength, true};
+}
+
+inline bool isShipDestroyed(BAShip ship){
+  return ship.remainingLength <= 0;
+}
+
+#endif
