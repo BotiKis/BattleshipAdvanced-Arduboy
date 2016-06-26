e2c21f16b49b855c824eb97272e897049da77be8
BKaaa <botond.kis@allaboutapps.at>
2016-06-26 21:46:03 +0200

    added new assets and right aligned text function

--- BattleshipAdvanced/src/BAUI.cpp
+++ BattleshipAdvanced/src/BAUI.cpp
@@ -2,7 +2,14 @@
 #include "BAUI.h"
 
 void drawText(const char *text, uint8_t x, uint8_t y, uint8_t color, Arduboy &arduboy){
-  for(byte i = 0; i < strlen(text); i++){
+  for(uint8_t i = 0; i < strlen(text); i++){
     arduboy.drawChar(x + i*6, y, text[i], color, (color+1)%2, 1);
+  }
+}
+
+void drawTextRightAligned(const char *text, uint8_t x, uint8_t y, uint8_t color, Arduboy &arduboy){
+  uint8_t len = strlen(text) -1;
+  for(uint8_t i = 0; i <= len; i++){
+    arduboy.drawChar(x - i*6, y, text[len - i], color, (color+1)%2, 1);
   }
 }
