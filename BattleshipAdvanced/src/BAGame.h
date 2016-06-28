02e95cd525b1405dca860ad2d1c58492571ae408
BKaaa <botond.kis@allaboutapps.at>
2016-06-29 00:03:43 +0200

    comments in BAGame.h

--- BattleshipAdvanced/src/BAGame.h
+++ BattleshipAdvanced/src/BAGame.h
@@ -56,14 +56,17 @@ private:
     BAGameCommand showCharSelection();  // Implemented in BAGameCharacterSelect.cpp
     BAGameCommand showVersusScreen();   // Implemented in BAGameVersusScreen.cpp
 
-    BAGameCommand showPlaceShips();     // Implemented in BAGamePlaceShips.cpp
-    void showPlaceShipsInfo(); // Implemented in BAGamePlaceShips.cpp
-    BAGameCommand showPlaceShipsMap();  // Implemented in BAGamePlaceShips.cpp
+    // Implemented in BAGamePlaceShips.cpp
+    BAGameCommand showPlaceShips();
+    void showPlaceShipsInfo();
+    BAGameCommand showPlaceShipsMap();
 
-    void showPrepareForGame();          // Implemented in BAGameShowStartingPlayer.cpp
+    // Implemented in BAGameShowStartingPlayer.cpp
+    void showPrepareForGame();
 
-    BAGameCommand playGame();           // Implemented in BACoreGame.cpp
-    BAGameCommand animateFromPlayerToPlayer();
+    // Implemented in BACoreGame.cpp
+    BAGameCommand playGame();
+    void animateFromPlayerToPlayer(BAPlayer*, BAPlayer*, bool);
     BAGameCommand showPlayerTurn();
     BAGameCommand showOpponentTurn();
 
